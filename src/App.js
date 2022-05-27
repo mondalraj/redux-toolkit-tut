070a025b599bd65985e7414d758014657fce25da
Rajib Mondal <mondal021002rajib@gmail.com>
2022-05-27 13:18:16 +0530

    Created a basic counter application using redux-toolkit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { decrement, increment, incrementByAmount } from "./redux/counter";
 
 function App() {
-  const [count, setCount] = useState(0);
+  //Read state
+  const { count } = useSelector((state) => state.counter);
+  // const count = useSelector((state) => state.counter.count);
+
+  //Get the actions
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <h1>Count: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(10))}>
+        Increase by 10
+      </button>
     </div>
   );
 }
